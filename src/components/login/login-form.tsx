457db5602d5e31b2098fdafdeea44cda7d9b2cca
Gabriel Brandao <gabrielbrandaosales@hotmail.com>
2024-07-24 10:47:12 -0300

    feat: input error component

--- src/components/login/login-form.tsx
+++ src/components/login/login-form.tsx
@@ -3,6 +3,9 @@
 import login from '@/actions/login';
 import { useFormState, useFormStatus } from 'react-dom';
 import Button from '@/components/forms/button';
+import Input from '@/components/forms/input';
+import ErrorMessage from '../helper/error-message';
+import React from 'react';
 
 function FormButton() {
   const { pending } = useFormStatus();
@@ -24,13 +27,17 @@ export default function LoginForrm() {
     error: '',
     data: null,
   });
+
+  React.useEffect(() => {
+    if (state.ok) window.location.href = '/conta';
+  }, [state]);
   return (
     <>
       <form action={action}>
-        <input type="text" name="username" placeholder="usuário" />
-        <input type="password" name="password" placeholder="senha" />
+        <Input label="Usuário" name="username" type="text" />
+        <Input label="Senha" name="password" type="password" />
+        <ErrorMessage error={state.error} />
         <FormButton />
-        <p>{state.error}</p>
       </form>
     </>
   );
