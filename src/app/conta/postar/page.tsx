c02202a9ca9e2b3d8294589c4053d8c5deae74fd
Gabriel Brandao <gabrielbrandaosales@hotmail.com>
2024-08-05 22:45:46 -0300

    feat: photo post

--- src/app/conta/postar/page.tsx
+++ src/app/conta/postar/page.tsx
@@ -1,7 +1,12 @@
+import ContaPhotoPost from '@/components/conta/conta-photo-post';
+import { Metadata } from 'next';
+
+export const runtime = 'edge';
+
+export const metadata: Metadata = {
+  title: 'Postar | Minha Conta',
+};
+
 export default async function ContaPostar() {
-  return (
-    <main>
-      <h1>Postar</h1>
-    </main>
-  );
+  return <ContaPhotoPost />;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import photosGet from '@/actions/photos-get';
 import Feed from '@/components/feed/feed';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Minha Conta',
+};
 
 export default async function Home() {
   const data = await photosGet();
