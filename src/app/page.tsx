f6ac9f8f20111f6cc41361b8fa103924d84fd293
Gabriel Brandao <gabrielbrandaosales@hotmail.com>
2024-08-10 18:50:01 -0300

    feat: feed conta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,11 @@
 import photosGet from '@/actions/photos-get';
 import Feed from '@/components/feed/feed';
-import { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: 'Minha Conta',
-};
 
 export default async function Home() {
   const data = await photosGet();
   return (
     <section className="container mainContainer">
-      <Feed fotos={data} />
+      <Feed photos={data} />
     </section>
   );
 }
