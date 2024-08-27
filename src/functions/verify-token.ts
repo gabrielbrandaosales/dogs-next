41eda5ba671b4a8f910bb5e9a3470b8c4d896452
Gabriel Brandao <gabrielbrandaosales@hotmail.com>
2024-08-27 15:39:36 -0300

    feat: add .env

--- src/functions/verify-token.ts
+++ src/functions/verify-token.ts
@@ -3,7 +3,7 @@ import { jwtVerify } from 'jose';
 export default async function verifyToken(token: string): Promise<boolean> {
   if (!token) return false;
   try {
-    // await jwtVerify(token, new TextEncoder().encode('aaaaa'), {
+    // await jwtVerify(token, new TextEncoder().encode(process.env.JWT_SALT), {
     //   algorithms: ['HS256'],
     // });
     return true;
